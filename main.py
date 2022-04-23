49cae657a14e10ea1c3d7f0c7df99dc8b9302481
Shreshth Tuli <shreshthtuli@gmail.com>
2022-04-23 19:49:57 +0100

    Added two direction surrogates

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def load_model(modelname, inp, out):
     import src.models
     model_class = getattr(src.models, modelname)
     model = model_class(inp.shape[1], out.shape[1], 32).double()
-    optimizer = torch.optim.Adam(model.parameters() , lr=1e-4, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters() , lr=1e-3, weight_decay=1e-5)
     fname = f'{checkpoints_folder}/{args.dataset}/{args.model}.ckpt'
     if os.path.exists(fname) and (not args.retrain or args.test):
         print(f"{color.GREEN}Loading pre-trained model: {model.name}{color.ENDC}")
@@ -53,41 +53,41 @@ def backprop(epoch, model, optimizer, dataloader):
     lf = nn.MSELoss(reduction = 'mean')
     ls = []
     for inp, out in tqdm(dataloader, leave=False, ncols=80):
-        pred = model(inp)
-        loss = lf(pred, out)
+        pred_i, pred_o = model(inp, out)
+        loss = lf(pred_o, out) + lf(pred_i, inp)
         ls.append(loss.item())   
         optimizer.zero_grad(); loss.backward(); optimizer.step()
     return np.mean(ls)
 
 def opt(model, dataloader):
     lf = nn.MSELoss(reduction = 'mean')
-    ls = []; new_inp = []
-    for init, out in tqdm(dataloader, leave=False, ncols=80):
+    ls = []; new_inp, new_out = [], []
+    for inp, out in tqdm(dataloader, leave=False, ncols=80):
         # update input
-        init.requires_grad = True
-        optimizer = torch.optim.AdamW([init] , lr=0.05)
+        inp.requires_grad = True; out.requires_grad = True
+        optimizer = torch.optim.Adam([inp, out] , lr=0.0001)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
         iteration = 0; equal = 0; z_old = 100
-        while iteration < 600:
-            init_old = deepcopy(init.data)
-            pred = model(init)
-            z = lf(pred, out)
+        while iteration < 800:
+            init_old = deepcopy(torch.cat([inp,out]).data)
+            pred_i, pred_o = model(inp, out)
+            z = lf(pred_o, out) + lf(pred_i, inp)
             optimizer.zero_grad(); z.backward(); optimizer.step(); scheduler.step()
-            init.data = scale(init.data)
-            equal = equal + 1 if torch.all(init_old - init < 0.01) else 0
+            inp.data, out.data = scale(inp.data), scale(out.data)
+            equal = equal + 1 if torch.all(init_old - torch.cat([inp,out]) < 0.01) else 0
             if equal > 30: break
             iteration += 1; z_old = z.item()
         ls.append(z.item())
-        init.requires_grad = False
-        new_inp.append(init)
-    return torch.cat(new_inp), np.mean(ls)
+        inp.requires_grad = False; out.requires_grad = False
+        new_inp.append(inp); new_out.append(out)
+    return torch.cat(new_inp), torch.cat(new_out), np.mean(ls)
  
 if __name__ == '__main__':
     inp, out, inp_c, out_c = load_data(args.dataset)
     inp_c, out_c = init_impute(inp_c, out_c, strategy = 'zero')
     model, optimizer, epoch, accuracy_list = load_model(args.model, inp, out)
 
-    num_epochs = 5 if not args.test else 0
+    num_epochs = 20 if not args.test else 0
     lf = nn.MSELoss(reduction = 'mean')
     
     for e in tqdm(list(range(epoch+1, epoch+num_epochs+1)), ncols=80):
@@ -98,11 +98,11 @@ if __name__ == '__main__':
         unfreeze_model(model)
         loss = backprop(e, model, optimizer, dataloader)
         accuracy_list.append(loss)
-        tqdm.write(f'Epoch {e},\tLoss = {loss}')
         save_model(model, optimizer, e, accuracy_list)
 
         # Tune Data
         freeze_model(model)
-        inp_c, dev = opt(model, dataloader)
-        tqdm.write(f'Epoch {e},\tMSE = {dev}')      
+        inp_c, out_c, loss = opt(model, dataloader)
+        dev = lf(inp_c, inp) + lf(out_c, out)
+        tqdm.write(f'Epoch {e},\tLoss = {loss},\tMSE = {dev}')      
         
--- src/models.py
+++ src/models.py
@@ -16,6 +16,29 @@ class FCN(nn.Module):
             nn.Linear(self.n_hidden, self.n_out), nn.Sigmoid(),
         )
 
-    def forward(self, inp):
-        out = self.fcn(inp)
-        return out
+    def forward(self, inp, out):
+        out2 = self.fcn(inp)
+        return inp, out2
+
+class FCN2(nn.Module):
+    def __init__(self, inp_size, out_size, n_hidden):
+        super(FCN2, self).__init__()
+        self.name = 'FCN2'
+        self.n_hidden = n_hidden
+        self.n_inp = inp_size
+        self.n_out = out_size
+        self.fcn = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.n_inp, self.n_hidden), nn.ReLU(True),
+            nn.Linear(self.n_hidden, self.n_out), nn.Sigmoid(),
+        )
+        self.fcn_reverse = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.n_out, self.n_hidden), nn.ReLU(True),
+            nn.Linear(self.n_hidden, self.n_inp), nn.Sigmoid(),
+        )
+
+    def forward(self, inp, out):
+        out2 = self.fcn(inp)
+        inp2 = self.fcn_reverse(out)
+        return inp2, out2
