676e72c4e87b50120629eeb28e2052793a0e11ff
shikhartuli <shikhartuli98@gmail.com>
2022-04-28 19:40:37 -0400

    Create grape.py

---
+++ grape.py
@@ -0,0 +1,380 @@
+import numpy as np
+from src.parser import *
+from src.utils import *
+from src.folderconstants import *
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import time
+import os
+import json
+from sklearn.metrics import mean_squared_error as mse
+from tqdm import tqdm
+
+import sys
+sys.path.append('./GRAPE/')
+import pickle
+import argparse
+import pandas as pd
+from uci.uci_data import get_data
+from mc.mc_subparser import add_mc_subparser
+from uci.uci_subparser import add_uci_subparser
+from models.gnn_model import get_gnn
+from models.prediction_model import MLPNet
+from utils.plot_utils import plot_curve, plot_sample
+from utils.utils import build_optimizer, objectview, get_known_mask, mask_edge
+
+
+# GRAPE trainer from: https://github.com/maxiaoba/GRAPE/blob/0ea0c59272a977d0184a8fd95178f68211455ef5/training/gnn_mdi.py
+def train_gnn_mdi(data, args, log_path, device=torch.device('cpu')):
+    model = get_gnn(data, args).to(device)
+    if args.impute_hiddens == '':
+        impute_hiddens = []
+    else:
+        impute_hiddens = list(map(int,args.impute_hiddens.split('_')))
+    if args.concat_states:
+        input_dim = args.node_dim * len(model.convs) * 2
+    else:
+        input_dim = args.node_dim * 2
+    if hasattr(args,'ce_loss') and args.ce_loss:
+        output_dim = len(data.class_values)
+    else:
+        output_dim = 1
+    impute_model = MLPNet(input_dim, output_dim,
+                            hidden_layer_sizes=impute_hiddens,
+                            hidden_activation=args.impute_activation,
+                            dropout=args.dropout).to(device)
+    if args.transfer_dir: # this ensures the valid mask is consistant
+        load_path = './{}/test/{}/{}/'.format(args.domain,args.data,args.transfer_dir)
+        print("loading fron {} with {}".format(load_path,args.transfer_extra))
+        model = torch.load(load_path+'model'+args.transfer_extra+'.pt',map_location=device)
+        impute_model = torch.load(load_path+'impute_model'+args.transfer_extra+'.pt',map_location=device)
+
+    trainable_parameters = list(model.parameters()) \
+                           + list(impute_model.parameters())
+    print("total trainable_parameters: ",len(trainable_parameters))
+    # build optimizer
+    scheduler, opt = build_optimizer(args, trainable_parameters)
+
+    # train
+    Train_loss = []
+    Test_rmse = []
+    Test_l1 = []
+    Lr = []
+
+    x = data.x.clone().detach().to(device)
+    if hasattr(args,'split_sample') and args.split_sample > 0.:
+        if args.split_train:
+            all_train_edge_index = data.lower_train_edge_index.clone().detach().to(device)
+            all_train_edge_attr = data.lower_train_edge_attr.clone().detach().to(device)
+            all_train_labels = data.lower_train_labels.clone().detach().to(device)
+        else:
+            all_train_edge_index = data.train_edge_index.clone().detach().to(device)
+            all_train_edge_attr = data.train_edge_attr.clone().detach().to(device)
+            all_train_labels = data.train_labels.clone().detach().to(device)
+        if args.split_test:
+            test_input_edge_index = data.higher_train_edge_index.clone().detach().to(device)
+            test_input_edge_attr = data.higher_train_edge_attr.clone().detach().to(device)
+        else:
+            test_input_edge_index = data.train_edge_index.clone().detach().to(device)
+            test_input_edge_attr = data.train_edge_attr.clone().detach().to(device)
+        test_edge_index = data.higher_test_edge_index.clone().detach().to(device)
+        test_edge_attr = data.higher_test_edge_attr.clone().detach().to(device)
+        test_labels = data.higher_test_labels.clone().detach().to(device)
+    else:
+        all_train_edge_index = data.train_edge_index.clone().detach().to(device)
+        all_train_edge_attr = data.train_edge_attr.clone().detach().to(device)
+        all_train_labels = data.train_labels.clone().detach().to(device)
+        test_input_edge_index = all_train_edge_index
+        test_input_edge_attr = all_train_edge_attr
+        test_edge_index = data.test_edge_index.clone().detach().to(device)
+        test_edge_attr = data.test_edge_attr.clone().detach().to(device)
+        test_labels = data.test_labels.clone().detach().to(device)
+    if hasattr(data,'class_values'):
+        class_values = data.class_values.clone().detach().to(device)
+    if args.valid > 0.:
+        valid_mask = get_known_mask(args.valid, int(all_train_edge_attr.shape[0] / 2)).to(device)
+        print("valid mask sum: ",torch.sum(valid_mask))
+        train_labels = all_train_labels[~valid_mask]
+        valid_labels = all_train_labels[valid_mask]
+        double_valid_mask = torch.cat((valid_mask, valid_mask), dim=0)
+        valid_edge_index, valid_edge_attr = mask_edge(all_train_edge_index, all_train_edge_attr, double_valid_mask, True)
+        train_edge_index, train_edge_attr = mask_edge(all_train_edge_index, all_train_edge_attr, ~double_valid_mask, True)
+        print("train edge num is {}, valid edge num is {}, test edge num is input {} output {}"\
+                .format(
+                train_edge_attr.shape[0], valid_edge_attr.shape[0],
+                test_input_edge_attr.shape[0], test_edge_attr.shape[0]))
+        Valid_rmse = []
+        Valid_l1 = []
+        best_valid_rmse = np.inf
+        best_valid_rmse_epoch = 0
+        best_valid_l1 = np.inf
+        best_valid_l1_epoch = 0
+    else:
+        train_edge_index, train_edge_attr, train_labels =\
+             all_train_edge_index, all_train_edge_attr, all_train_labels
+        print("train edge num is {}, test edge num is input {}, output {}"\
+                .format(
+                train_edge_attr.shape[0],
+                test_input_edge_attr.shape[0], test_edge_attr.shape[0]))
+    if args.auto_known:
+        args.known = float(all_train_labels.shape[0])/float(all_train_labels.shape[0]+test_labels.shape[0])
+        print("auto calculating known is {}/{} = {:.3g}".format(all_train_labels.shape[0],all_train_labels.shape[0]+test_labels.shape[0],args.known))
+    obj = dict()
+    obj['args'] = args
+    obj['outputs'] = dict()
+    for epoch in range(args.epochs):
+        model.train()
+        impute_model.train()
+        known_mask = get_known_mask(args.known, int(train_edge_attr.shape[0] / 2)).to(device)
+        double_known_mask = torch.cat((known_mask, known_mask), dim=0)
+        known_edge_index, known_edge_attr = mask_edge(train_edge_index, train_edge_attr, double_known_mask, True)
+
+        opt.zero_grad()
+        x_embd = model(x, known_edge_attr, known_edge_index)
+        pred = impute_model([x_embd[train_edge_index[0]], x_embd[train_edge_index[1]]])
+        if hasattr(args,'ce_loss') and args.ce_loss:
+            pred_train = pred[:int(train_edge_attr.shape[0] / 2)]
+        else:
+            pred_train = pred[:int(train_edge_attr.shape[0] / 2),0]
+        if args.loss_mode == 1:
+            pred_train[known_mask] = train_labels[known_mask]
+        label_train = train_labels
+
+        if hasattr(args,'ce_loss') and args.ce_loss:
+            loss = F.cross_entropy(pred_train,train_labels)
+        else:
+            loss = F.mse_loss(pred_train, label_train)
+        loss.backward()
+        opt.step()
+        train_loss = loss.item()
+        if scheduler is not None:
+            scheduler.step(epoch)
+        for param_group in opt.param_groups:
+            Lr.append(param_group['lr'])
+
+        model.eval()
+        impute_model.eval()
+        with torch.no_grad():
+            if args.valid > 0.:
+                x_embd = model(x, train_edge_attr, train_edge_index)
+                pred = impute_model([x_embd[valid_edge_index[0], :], x_embd[valid_edge_index[1], :]])
+                if hasattr(args,'ce_loss') and args.ce_loss:
+                    pred_valid = class_values[pred[:int(valid_edge_attr.shape[0] / 2)].max(1)[1]]
+                    label_valid = class_values[valid_labels]
+                elif hasattr(args,'norm_label') and args.norm_label:
+                    pred_valid = pred[:int(valid_edge_attr.shape[0] / 2),0]
+                    pred_valid = pred_valid * max(class_values)
+                    label_valid = valid_labels
+                    label_valid = label_valid * max(class_values)
+                else:
+                    pred_valid = pred[:int(valid_edge_attr.shape[0] / 2),0]
+                    label_valid = valid_labels
+                mse = F.mse_loss(pred_valid, label_valid)
+                valid_rmse = np.sqrt(mse.item())
+                l1 = F.l1_loss(pred_valid, label_valid)
+                valid_l1 = l1.item()
+                if valid_l1 < best_valid_l1:
+                    best_valid_l1 = valid_l1
+                    best_valid_l1_epoch = epoch
+                    if args.save_model:
+                        torch.save(model, log_path + 'model_best_valid_l1.pt')
+                        torch.save(impute_model, log_path + 'impute_model_best_valid_l1.pt')
+                if valid_rmse < best_valid_rmse:
+                    best_valid_rmse = valid_rmse
+                    best_valid_rmse_epoch = epoch
+                    if args.save_model:
+                        torch.save(model, log_path + 'model_best_valid_rmse.pt')
+                        torch.save(impute_model, log_path + 'impute_model_best_valid_rmse.pt')
+                Valid_rmse.append(valid_rmse)
+                Valid_l1.append(valid_l1)
+
+            x_embd = model(x, test_input_edge_attr, test_input_edge_index)
+            pred = impute_model([x_embd[test_edge_index[0], :], x_embd[test_edge_index[1], :]])
+            if hasattr(args,'ce_loss') and args.ce_loss:
+                pred_test = class_values[pred[:int(test_edge_attr.shape[0] / 2)].max(1)[1]]
+                label_test = class_values[test_labels]
+            elif hasattr(args,'norm_label') and args.norm_label:
+                pred_test = pred[:int(test_edge_attr.shape[0] / 2),0]
+                pred_test = pred_test * max(class_values)
+                label_test = test_labels
+                label_test = label_test * max(class_values)
+            else:
+                pred_test = pred[:int(test_edge_attr.shape[0] / 2),0]
+                label_test = test_labels
+            mse = F.mse_loss(pred_test, label_test)
+            test_rmse = np.sqrt(mse.item())
+            l1 = F.l1_loss(pred_test, label_test)
+            test_l1 = l1.item()
+            if args.save_prediction:
+                if epoch == best_valid_rmse_epoch:
+                    obj['outputs']['best_valid_rmse_pred_test'] = pred_test.detach().cpu().numpy()
+                if epoch == best_valid_l1_epoch:
+                    obj['outputs']['best_valid_l1_pred_test'] = pred_test.detach().cpu().numpy()
+
+            if args.mode == 'debug':
+                torch.save(model, log_path + 'model_{}.pt'.format(epoch))
+                torch.save(impute_model, log_path + 'impute_model_{}.pt'.format(epoch))
+            Train_loss.append(train_loss)
+            Test_rmse.append(test_rmse)
+            Test_l1.append(test_l1)
+            print('epoch: ', epoch)
+            print('loss: ', train_loss)
+            if args.valid > 0.:
+                print('valid rmse: ', valid_rmse)
+                print('valid l1: ', valid_l1)
+            print('test rmse: ', test_rmse)
+            print('test l1: ', test_l1)
+
+    pred_train = pred_train.detach().cpu().numpy()
+    label_train = label_train.detach().cpu().numpy()
+    pred_test = pred_test.detach().cpu().numpy()
+    label_test = label_test.detach().cpu().numpy()
+
+    obj['curves'] = dict()
+    obj['curves']['train_loss'] = Train_loss
+    if args.valid > 0.:
+        obj['curves']['valid_rmse'] = Valid_rmse
+        obj['curves']['valid_l1'] = Valid_l1
+    obj['curves']['test_rmse'] = Test_rmse
+    obj['curves']['test_l1'] = Test_l1
+    obj['lr'] = Lr
+
+    obj['outputs']['final_pred_train'] = pred_train
+    obj['outputs']['label_train'] = label_train
+    obj['outputs']['final_pred_test'] = pred_test
+    obj['outputs']['label_test'] = label_test
+    pickle.dump(obj, open(log_path + 'result.pkl', "wb"))
+
+    if args.save_model:
+        torch.save(model, log_path + 'model.pt')
+        torch.save(impute_model, log_path + 'impute_model.pt')
+
+    # obj = objectview(obj)
+    plot_curve(obj['curves'], log_path+'curves.png',keys=None, 
+                clip=True, label_min=True, label_end=True)
+    plot_curve(obj, log_path+'lr.png',keys=['lr'], 
+                clip=False, label_min=False, label_end=False)
+    plot_sample(obj['outputs'], log_path+'outputs.png', 
+                groups=[['final_pred_train','label_train'],
+                        ['final_pred_test','label_test']
+                        ], 
+                num_points=20)
+    if args.save_prediction and args.valid > 0.:
+        plot_sample(obj['outputs'], log_path+'outputs_best_valid.png', 
+                    groups=[['best_valid_rmse_pred_test','label_test'],
+                            ['best_valid_l1_pred_test','label_test']
+                            ], 
+                    num_points=20)
+    if args.valid > 0.:
+        print("best valid rmse is {:.3g} at epoch {}".format(best_valid_rmse,best_valid_rmse_epoch))
+        print("best valid l1 is {:.3g} at epoch {}".format(best_valid_l1,best_valid_l1_epoch))
+
+    x_embd = model(x, known_edge_attr, known_edge_index)
+    pred = impute_model([x_embd[train_edge_index[0]], x_embd[train_edge_index[1]]])
+    pred_train = pred[:int(train_edge_attr.shape[0] / 2),0]
+
+    x_embd_test = model(x, test_input_edge_attr, test_input_edge_index)
+    pred_test = impute_model([x_embd_test[test_edge_index[0], :], x_embd_test[test_edge_index[1], :]])
+    pred_test = pred_test[:int(test_edge_attr.shape[0] / 2),0]
+
+    return pred_train.detach().numpy(), pred_test.detach().numpy(), train_labels.detach().numpy(), test_labels.detach().numpy()   
+
+def load_data(dataset):
+    inp = torch.tensor(np.load(f'{output_folder}/{dataset}/inp.npy'), dtype=torch.float)
+    out = torch.tensor(np.load(f'{output_folder}/{dataset}/out.npy'), dtype=torch.float)
+    inp_c = torch.tensor(np.load(f'{output_folder}/{dataset}/inp_c.npy'), dtype=torch.float)
+    out_c = torch.tensor(np.load(f'{output_folder}/{dataset}/out_c.npy'), dtype=torch.float)
+    return inp, out, inp_c, out_c
+
+def init_impute(inp_c, out_c, inp_m, out_m, strategy = 'zero'):
+    if strategy == 'zero':
+        inp_r, out_r = torch.zeros(inp_c.shape), torch.zeros(out_c.shape)
+    elif strategy == 'random':
+        inp_r, out_r = torch.rand(inp_c.shape), torch.rand(out_c.shape)
+    else:
+        raise NotImplementedError()
+    inp_r, out_r = inp_r.double(), out_r.double()
+    inp_c[inp_m], out_c[out_m] = inp_r[inp_m], out_r[out_m]
+    return inp_c, out_c
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_types', type=str, default='EGSAGE_EGSAGE_EGSAGE')
+    parser.add_argument('--post_hiddens', type=str, default=None,) # default to be 1 hidden of node_dim
+    parser.add_argument('--concat_states', action='store_true', default=False)
+    parser.add_argument('--norm_embs', type=str, default=None,) # default to be all true
+    parser.add_argument('--aggr', type=str, default='mean',)
+    parser.add_argument('--node_dim', type=int, default=64)
+    parser.add_argument('--edge_dim', type=int, default=64)
+    parser.add_argument('--edge_mode', type=int, default=1)  # 0: use it as weight; 1: as input to mlp
+    parser.add_argument('--gnn_activation', type=str, default='relu')
+    parser.add_argument('--impute_hiddens', type=str, default='64')
+    parser.add_argument('--impute_activation', type=str, default='relu')
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--opt', type=str, default='adam')
+    parser.add_argument('--opt_scheduler', type=str, default='none')
+    parser.add_argument('--opt_restart', type=int, default=0)
+    parser.add_argument('--opt_decay_step', type=int, default=1000)
+    parser.add_argument('--opt_decay_rate', type=float, default=0.9)
+    parser.add_argument('--dropout', type=float, default=0.)
+    parser.add_argument('--weight_decay', type=float, default=0.)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--known', type=float, default=0.7) # 1 - edge dropout rate
+    parser.add_argument('--auto_known', action='store_true', default=False)
+    parser.add_argument('--loss_mode', type=int, default = 0) # 0: loss on all train edge, 1: loss only on unknown train edge
+    parser.add_argument('--valid', type=float, default=0.) # valid-set ratio
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--log_dir', type=str, default='log_dir')
+    parser.add_argument('--save_model', action='store_true', default=False)
+    parser.add_argument('--save_prediction', action='store_true', default=False)
+    parser.add_argument('--transfer_dir', type=str, default=None)
+    parser.add_argument('--transfer_extra', type=str, default='')
+    parser.add_argument('--mode', type=str, default='train') # debug
+    subparsers = parser.add_subparsers()
+    add_uci_subparser(subparsers)
+    add_mc_subparser(subparsers)
+    grape_args = parser.parse_args()
+
+    inp, out, inp_c, out_c = load_data(args.dataset)
+    inp_m, out_m = torch.isnan(inp_c).float(), torch.isnan(out_c).float()
+    # inp_c, out_c = init_impute(inp_c, out_c, inp_m, out_m, strategy = 'zero')
+
+    df_inp = pd.DataFrame(inp)
+    df_out = pd.DataFrame(out)
+
+    data_forward = get_data(df_inp, 
+        df_out, 
+        0, # node mode
+        0.7, # train edge
+        0, # split sample (percentage of model to corrupt)
+        'random', # split by in ['y', 'random']
+        0.9, # train_y prob.
+        0)
+
+    data_backward = get_data(df_out, 
+        df_inp, 
+        0, # node mode
+        0.7, # train edge
+        0.1, # split sample (percentage of model to corrupt)
+        'random', # split by in ['y', 'random']
+        0.9, # train_y prob.
+        0)
+
+    log_path = './temp/{}/'.format(grape_args.log_dir)
+    os.makedirs(log_path, exist_ok=True)
+
+    print('Training forward model...')
+    pred_train1, pred_test1, train_labels1, test_labels1 = train_gnn_mdi(data_forward, grape_args, log_path, torch.device('cpu'))
+
+    print('Training backward model...')
+    grape_args.epochs = 1
+    pred_train2, pred_test2, train_labels2, test_labels2 = train_gnn_mdi(data_backward, grape_args, log_path, torch.device('cpu'))
+
+    pred = np.concatenate([pred_train1, pred_test1, pred_train2, pred_test2])
+    labels = np.concatenate([train_labels1, test_labels1, train_labels2, test_labels2])
+
+    print('Final MSE: ', mse(pred, labels))
