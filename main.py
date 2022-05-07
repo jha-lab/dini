e34976479c55de306984e312a292d73e9f37e302
shikhartuli <shikhartuli98@gmail.com>
2022-05-07 15:40:47 -0400

    Create main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.utils import *
 from src.folderconstants import *
+from src.models import *
 import argparse
 import torch
 import numpy as np
@@ -15,6 +16,8 @@ from baseline import load_data as load_data_all, init_impute as init_impute_all
 from gmm import correct_subset, opt as gmm_opt
 from sklearn.mixture import GaussianMixture
 from grape import load_data as load_data_sep, init_impute as init_impute_sep, train_gnn_mdi
+from gain import GAINGenerator, GAINDiscriminator, GAINTrainer
+from dini import load_model, save_model, backprop, opt
 
 import sys
 sys.path.append('./GRAPE/')
@@ -29,15 +32,19 @@ from models.prediction_model import MLPNet
 from utils.plot_utils import plot_curve, plot_sample
 from utils.utils import build_optimizer, objectview, get_known_mask, mask_edge
 
+from matplotlib import pyplot as plt
+
 import warnings
+from functools import partialmethod
 warnings.filterwarnings('ignore')
+# tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DINI')
     parser.add_argument('--dataset', 
                         metavar='-d', 
                         type=str, 
-                        required=True,
+                        required=False,
                         default='MSDS',
                         help="dataset from ['MSDS', 'concrete', 'linear']")
     parser.add_argument('--strategy', 
@@ -104,6 +111,24 @@ if __name__ == '__main__':
     print(f'GMM MSE:\t', mse(data[data_m], data_new[data_m]))
     print(f'GMM MAE:\t', mae(data[data_m], data_new[data_m]))
 
+    results['gmm'] = [mse(data[data_m], data_new[data_m]), mae(data[data_m], data_new[data_m])]
+
+    # Run GAIN
+    inp, out, inp_c, out_c = load_data_sep(args.dataset)
+    inp_m, out_m = torch.isnan(inp_c), torch.isnan(out_c)
+    inp_c_imputed, out_c_imputed = init_impute_sep(inp_c, out_c, inp_m, out_m, strategy = 'zero')
+    inp_m, out_m = inp_c.float(), out_c.float()
+
+    dataloader = DataLoader(list(zip(inp, inp_c, out_c, (1-inp_m))), batch_size=128, shuffle=False)
+    
+    trainer = GAINTrainer(inp_c.shape[1], out_c.shape[1], {'min': torch.zeros(inp_c.shape[1]), 'max': torch.ones(inp_c.shape[1])}, args)
+    perf_dict, imputed_data = trainer.train_model(dataloader, dataloader)
+    data_new = torch.cat((imputed_data, out_c_imputed), dim=1)
+    print(f'GAIN MSE:\t', mse(data[data_m], data_new[data_m]))
+    print(f'GAIN MAE:\t', mae(data[data_m], data_new[data_m]))
+
+    results['gain'] = [mse(data[data_m], data_new[data_m]), mae(data[data_m], data_new[data_m])]
+
     # Run GRAPE
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_types', type=str, default='EGSAGE_EGSAGE_EGSAGE')
@@ -137,15 +162,14 @@ if __name__ == '__main__':
     parser.add_argument('--transfer_dir', type=str, default=None)
     parser.add_argument('--transfer_extra', type=str, default='')
     parser.add_argument('--mode', type=str, default='train') # debug
+    parser.add_argument('--dataset', type=str, default=args.dataset)
+    parser.add_argument('--strategy', type=str, default=args.strategy)
+    parser.add_argument('--fraction', type=str, default=args.fraction)
     subparsers = parser.add_subparsers()
     add_uci_subparser(subparsers)
     add_mc_subparser(subparsers)
     grape_args = parser.parse_args()
-    print(grape_args)
-
-    inp, out, inp_c, out_c = load_data_sep(grape_args.dataset)
-    inp_m, out_m = torch.isnan(inp_c).float(), torch.isnan(out_c).float()
-    # inp_c, out_c = init_impute(inp_c, out_c, inp_m, out_m, strategy = 'zero')
+    grape_args.dataset = args.dataset
 
     df_inp = pd.DataFrame(inp)
     df_out = pd.DataFrame(out)
@@ -172,14 +196,74 @@ if __name__ == '__main__':
     os.makedirs(log_path, exist_ok=True)
 
     # print('Training forward model...')
-    pred_train1, pred_test1, train_labels1, test_labels1 = train_gnn_mdi(data_forward, grape_args, log_path, torch.device('cpu'))
+    pred_train1, pred_test1, train_labels1, test_labels1 = train_gnn_mdi(data_forward, grape_args, log_path, torch.device('cpu'), verbose=False)
 
     # print('Training backward model...')
     grape_args.epochs = 1
-    pred_train2, pred_test2, train_labels2, test_labels2 = train_gnn_mdi(data_backward, grape_args, log_path, torch.device('cpu'))
+    pred_train2, pred_test2, train_labels2, test_labels2 = train_gnn_mdi(data_backward, grape_args, log_path, torch.device('cpu'), verbose=False)
 
     pred = np.concatenate([pred_train1, pred_test1, pred_train2, pred_test2])
     labels = np.concatenate([train_labels1, test_labels1, train_labels2, test_labels2])
 
-    print('GRAPE MSE: ', mse(pred, labels))
-    print('GRAPE MAE: ', mae(pred, labels))
+    print('GRAPE MSE:\t', mse(pred, labels))
+    print('GRAPE MAE:\t', mae(pred, labels))
+
+    results['grape'] = [mse(data[data_m], data_new[data_m]), mae(data[data_m], data_new[data_m])]
+
+    # Run DINI
+    num_epochs = 30
+    lf = nn.MSELoss(reduction = 'mean')
+
+    inp, out, inp_c, out_c = load_data_sep(args.dataset)
+    inp_m, out_m = torch.isnan(inp_c), torch.isnan(out_c)
+    inp_m2, out_m2 = torch.logical_not(inp_m), torch.logical_not(out_m)
+    inp_c, out_c = init_impute_sep(inp_c, out_c, inp_m, out_m, strategy = 'zero')
+    inp_c, out_c = inp_c.double(), out_c.double()
+    model, optimizer, epoch, accuracy_list = load_model('FCN2', inp, out, args.dataset, True, False)
+    data_c = torch.cat([inp_c, out_c], dim=1)
+    data_m = torch.cat([inp_m, out_m], dim=1)
+    data = torch.cat([inp, out], dim=1)
+
+    for e in tqdm(list(range(epoch+1, epoch+num_epochs+1)), ncols=80):
+        # Get Data
+        dataloader = DataLoader(list(zip(inp_c, out_c, inp_m, out_m)), batch_size=1, shuffle=False)
+
+        # Tune Model
+        unfreeze_model(model)
+        loss = backprop(e, model, optimizer, dataloader)
+        accuracy_list.append(loss)
+        save_model(model, optimizer, e, accuracy_list, args.dataset, 'FCN2')
+
+        # Tune Data
+        freeze_model(model)
+        inp_c, out_c = opt(model, dataloader)
+        data_c = torch.cat([inp_c, out_c], dim=1)
+        tqdm.write(f'Epoch {e},\tLoss = {loss},\tMSE = {lf(data[data_m], data_c[data_m]).item()},\tMAE = {mae(data[data_m].detach().numpy(), data_c[data_m].detach().numpy())}')  
+
+    print('DINI MSE:\t', lf(data[data_m], data_c[data_m]).item())
+    print('DINI MSE\t', mae(data[data_m].detach().numpy(), data_c[data_m].detach().numpy()))
+
+    results['dini'] = [lf(data[data_m], data_c[data_m]).item(), mae(data[data_m].detach().numpy(), data_c[data_m].detach().numpy())]
+
+    os.makedirs('./results', exist_ok=True)
+    df = pd.DataFrame(results)
+    df.index = ['mse', 'mae']
+
+    fraction_str = str(args.fraction).split(".")[-1]
+    df.to_csv(f'./results/{args.dataset.lower()}_{args.strategy.lower()}_p{fraction_str}.csv')
+
+    fig, ax = plt.subplots(figsize=(18, 4.8))
+    x = np.arange(len(results))
+    width = 0.4
+
+    # ax.bar(x, [results[key][0] for key in results.keys()], color='#4292C6', label='MSE')
+    ax.bar(x - width*0.5, [results[key][0] for key in results.keys()], width, color='#F1C40F', label='MSE')
+    ax.bar(x + width*0.5, [results[key][1] for key in results.keys()], width, color='#E67E22', label='MAE')
+    ax.set_ylabel('Error')
+    ax.set_xticks(x)
+    ax.set_xticklabels([key.upper() for key in results.keys()])
+    ax.legend(loc='upper right')
+    plt.title(f'Dataset: {args.dataset.upper()} | Corruption strategy: {args.strategy.upper()} | Fraction: {args.fraction}')
+    plt.grid(axis='y', linestyle='--')
+    plt.savefig(f'./results/{args.dataset.lower()}_{args.strategy.lower()}_p{fraction_str}.pdf', bbox_inches='tight')
+
