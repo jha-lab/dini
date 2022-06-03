53bd595947b16fb79d21b6173f26674593e4c0b7
shikhartuli <shikhartuli98@gmail.com>
2022-06-03 17:04:37 +0530

    Added SWaT dataset

---
+++ data/swat/clean.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+# Dataset source: https://itrust.sutd.edu.sg/itrust-labs_datasets/dataset_info/
+
+# Read csv file
+
+if not path.exists('./SWaT_Dataset_Attack_v0.csv'): 
+	raise RuntimeError('Raw dataset file not found. Request access from: https://itrust.sutd.edu.sg/itrust-labs_datasets/dataset_info/')
+	
+df = pd.read_csv('./SWaT_Dataset_Attack_v0.csv')
+
+# Process dataframe
+
+df = df.iloc[:, 1:]
+df['Normal/Attack'] = df['Normal/Attack'].map({'Normal': 0, 'Attack': 1})
+df = df.sample(frac=0.5, random_state=0)
+
+nunique = df.nunique()
+cols_to_drop = nunique[nunique == 1].index
+df = df.drop(cols_to_drop, axis=1)
+
+df = df.dropna()
+
+# Save dataframe
+
+df.to_csv('./data.csv', index=False)
+
+
