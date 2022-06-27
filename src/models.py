0cc03fe31e5be1c28f5d9c0c8a1c846004d00a6c
shikhartuli <shikhartuli98@gmail.com>
2022-06-27 22:24:32 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -18,6 +18,9 @@ class FCN(nn.Module):
             nn.Linear(self.n_hidden, self.n_out), nn.Sigmoid(),
         )
 
+    def num_params(self):
+        return sum(p.numel() for p in self.fcn.parameters() if p.requires_grad)
+
     def forward(self, inp, out):
         out2 = self.fcn(inp)
         return inp, out2
@@ -42,13 +45,16 @@ class FCN2(nn.Module):
             nn.Linear(self.n_hidden, self.n_inp), nn.Sigmoid(),
         )
 
+    def num_params(self):
+        return sum(p.numel() for p in self.fcn.parameters() if p.requires_grad) + sum(p.numel() for p in self.fcn_reverse.parameters() if p.requires_grad)
+
     def forward(self, inp, out):
         out2 = 2 * self.fcn(inp) - 0.5
         inp2 = 2 * self.fcn_reverse(out) - 0.5
         return inp2, out2
 
 class LSTM2(nn.Module):
-    def __init__(self, inp_size, out_size, n_hidden):
+    def __init__(self, inp_size, out_size, n_hidden, num_layers=1, bidirectional=False):
         super(LSTM2, self).__init__()
         self.name = 'LSTM2'
         self.hidden_size = n_hidden
@@ -57,13 +63,18 @@ class LSTM2(nn.Module):
         self.lstm = nn.LSTM(input_size=self.inp_size,
             hidden_size=self.hidden_size,
             proj_size=self.out_size,
-            # bidirectional=True,
+            bidirectional=False,
+            num_layers=num_layers,
             batch_first=True)
         self.lstm_reverse = nn.LSTM(input_size=self.out_size,
             hidden_size=self.hidden_size,
             proj_size=self.inp_size,
-            # bidirectional=True,
+            bidirectional=False,
+            num_layers=num_layers,
             batch_first=True)
+
+    def num_params(self):
+        return sum(p.numel() for p in self.lstm.parameters() if p.requires_grad) + sum(p.numel() for p in self.lstm_reverse.parameters() if p.requires_grad)
 
     def forward(self, inp, out):
         out2, _ = self.lstm(inp)
@@ -71,7 +82,7 @@ class LSTM2(nn.Module):
         return inp2, out2
 
 class TXF2(nn.Module):
-    def __init__(self, inp_size, out_size, n_hidden, use_pos_emb=False):
+    def __init__(self, inp_size, out_size, n_hidden, num_layers=1, num_attn_heads=4, use_pos_emb=True):
         super(TXF2, self).__init__()
         self.name = 'TXF2'
         self.hidden_size = n_hidden
@@ -81,19 +92,28 @@ class TXF2(nn.Module):
         self.pos_emb_reverse = nn.Linear(self.out_size, self.out_size)
         self.use_pos_emb = use_pos_emb
         self.txf = nn.Sequential(
-            nn.TransformerEncoderLayer(d_model=self.inp_size,
-                nhead=1,
-                dim_feedforward=self.hidden_size,
-                batch_first=True),
-            nn.Linear(self.inp_size, self.out_size)
+            nn.Linear(self.inp_size, self.hidden_size),
+            nn.TransformerEncoder(
+                nn.TransformerEncoderLayer(d_model=self.hidden_size,
+                    nhead=num_attn_heads,
+                    dim_feedforward=4*self.hidden_size,
+                    batch_first=True),
+                num_layers=num_layers),
+            nn.Linear(self.hidden_size, self.out_size)
             )
         self.txf_reverse = nn.Sequential(
-            nn.TransformerEncoderLayer(d_model=self.out_size,
-                nhead=1,
-                dim_feedforward=self.hidden_size,
-                batch_first=True),
-            nn.Linear(self.out_size, self.inp_size)
+            nn.Linear(self.out_size, self.hidden_size),
+            nn.TransformerEncoder(
+                nn.TransformerEncoderLayer(d_model=self.hidden_size,
+                    nhead=num_attn_heads,
+                    dim_feedforward=4*self.hidden_size,
+                    batch_first=True),
+                num_layers=num_layers),
+            nn.Linear(self.hidden_size, self.inp_size)
             )
+
+    def num_params(self):
+        return sum(p.numel() for p in self.txf.parameters() if p.requires_grad) + sum(p.numel() for p in self.txf_reverse.parameters() if p.requires_grad)
 
     def forward(self, inp, out):
         if self.use_pos_emb:
